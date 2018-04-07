fab2c649ce58fe161e760ab572bed98d7b5658d2
csklimowski <csklimowski@email.arizona.edu>
2018-04-07 00:50:11 -0700

    basic demo image

--- src/game.js
+++ src/game.js
@@ -1,6 +1,6 @@
 import { MainState } from './states/main';
 
-var game = new Phaser.Game(600, 400, Phaser.AUTO, 'game');
+var game = new Phaser.Game(1280, 720, Phaser.AUTO, 'game');
 
 game.state.add('main', MainState);
 
