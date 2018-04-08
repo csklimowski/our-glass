dc7648da9ab47b08dea5ead83c1bbb9bb5aba6ad
cwheeler1997 <cwheeler1997@email.arizona.edu>
2018-04-07 18:03:03 -0700

    Cactus some more!

--- src/objects/cactus.js
+++ src/objects/cactus.js
@@ -6,13 +6,34 @@ export class Cactus extends Phaser.Sprite {
         super(game, 0, 0, 'cactus');
         game.add.existing(this);
 
-        this.scale.set(.25);
+        this.scale.set(.2);
+        this.anchor.set(0.5, 1);
+
+        let a = glassWidth(game.timer);
+        let b = glassHeight(game.timer);
         
-        this.x = (game.width/2 - 400) + Math.random() * 800;
-        this.y = (game.height/2 - 400) + Math.random() * 800;
+        this.x = (game.width/2 - a) + Math.random() * (a * 2);
+        this.y = sandPos(game.timer) - (b/2) + Math.random() * (b * 2);
+
+        let angleToCenter = Math.atan2(this.y, this.x);
+        let r = (a*b)/Math.sqrt(a*a*Math.sin(angleToCenter)*Math.sin(angleToCenter) + b*b*Math.cos(b)*Math.cos(b));
+        let linelength = Math.sqrt((x-a)*(x-a) + (y-b)*(y-b));
+        if(lineLength > r) {
+            
+        }
+
+
+        // let sandRadius = glassWidth(game.timer);
+        // let distFromCenter = Math.sqrt(this.x*this.x + this.y*this.y);
+        // if (distFromCenter > sandRadius) {
+        //     let angleToCenter = Math.atan2(this.y, this.x);
+        //     this.y = Math.sin(angleToCenter)*sandRadius;
+        //     this.x = Math.cos(angleToCenter)*sandRadius;
+        // }
+        
     }
 
     update() { 
-        
+
     }
 }
