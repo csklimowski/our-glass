2749a12870b36671d57da3333951b08ae510f270
cwheeler1997 <cwheeler1997@email.arizona.edu>
2018-04-07 23:24:14 -0700

    added sounds to load

--- src/states/load.js
+++ src/states/load.js
@@ -8,6 +8,12 @@ export class LoadState extends Phaser.State {
         game.load.image('anchor', 'img/anchor.png');
         game.load.image('light-sand-particle', 'image/light-sand-particle.png');
         game.load.spritesheet('sway', 'img/sprites/sway.png', 150, 150);
+        game.load.audio('hit_chest','audio/anchor_hit_chest.ogg');
+        game.load.audio('hit_glass','audio/anchor_hit_glass.ogg');
+        game.load.audio('hit_ground','audio/anchor_hit_ground.ogg');
+        game.load.audio('bury_chest','audio/bury_chest.ogg');
+        game.load.audio('anchor_fall','audio/falling_anchor.ogg');
+        game.load.audio('flip', 'hourglass_flip.ogg');
     }
 
     create() {
@@ -16,5 +22,14 @@ export class LoadState extends Phaser.State {
         game.scale.pageAlignHorizontally = true;
         game.scale.pageAlignVertically = true;
         game.state.start('title');
+
+        game.sfx = {
+            hit_chest: game.add.sound('hit_chest'),
+            hit_glass: game.add.sound('hit_glass'),
+            hit_ground: game.add.sound('hit_ground'),
+            bury: game.add.sound('bury_chest'),
+            fall: game.add.sound('anchor_fall'),
+            flip: game.add.sound('flip')
+        };
     }
 }
