f51a558f400014a3f5459e0da0b9ec198d9ff804
cwheeler1997 <cwheeler1997@email.arizona.edu>
2018-04-07 21:31:29 -0700

    fixed layers

--- src/states/main.js
+++ src/states/main.js
@@ -11,25 +11,33 @@ export class MainState extends Phaser.State {
         game.stage.backgroundColor = 0x55aaee;
 		this.graphics = game.add.graphics(0, 0);
 		game.timer = 0.4;
+		this.group = game.add.group();
+		this.group2 = game.add.group();
+		let g = this.group;
 		this.sand = new Sand();
+		this.group2.add(this.sand);
 		this.hider = new ControlledHider();
-		new Anchor();
-		new WanderingHider();
+		g.add(this.hider);
+		g.add(new Anchor());
+		g.add(new WanderingHider());
 		this.cacti = [ new Cactus(100) , new Cactus(250) , new Cactus(400) ];
 		this.spawnClock = Math.random();
-		for(let i = 0; i < 3; i++) {
-			for(let j = i + 1; j < 3; j++)
-			{
-				if(this.cacti[i].y > this.cacti[j].y){
-					let temp = this.cacti[i];
-					this.cacti[i] = this.cacti[j];
-					this.cacti[j] = temp;
-				}
-			}
-		}
+		// for(let i = 0; i < 3; i++) {
+		// 	for(let j = i + 1; j < 3; j++)
+		// 	{
+		// 		if(this.cacti[i].y > this.cacti[j].y){
+		// 			let temp = this.cacti[i];
+		// 			this.cacti[i] = this.cacti[j];
+		// 			this.cacti[j] = temp;
+		// 		}
+		// 	}
+		// }
 		for(let i = 0; i < 3; i++) {
 			game.add.existing(this.cacti[i]);
+			g.add(this.cacti[i]);
 		}
+		game.world.bringToTop(this.group);
+		g.sort('py', Phaser.Group.SORT_ASCENDING);
 	}
 
 	update() {
@@ -42,6 +50,7 @@ export class MainState extends Phaser.State {
 			this.spawnClock = 1.0 + Math.random()*0.5;
 		}
 
+		this.group.sort('py', Phaser.Group.SORT_ASCENDING);
 		// let h = this.hider;
 		// let s = this.sand;
 		// h.x = 0.5*Math.cos(game.time.now/1000);
