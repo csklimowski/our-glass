0a98c806b48ffe377fb06a8a065e99d8fbbcd58f
Chris Klimowski <csklimowski@gmail.com>
2020-05-21 22:29:03 -0700

    cactus things

--- src/objects/cactus.js
+++ src/objects/cactus.js
@@ -3,7 +3,7 @@ import { glassWidth, glassHeight, sandPos } from '../util/math';
 
 export class Cactus extends Phaser.Sprite {
     constructor(disappear){
-        super(game, 0, 0, 'cactus_dance');
+        super(game, 0, 0, 'cactus-dance');
         game.add.existing(this);
         
         this.animations.add('dance', _.range(60), 30, true);
@@ -15,7 +15,7 @@ export class Cactus extends Phaser.Sprite {
         this.anchor.set(0.75, .9);
 
         this.angleToCenter = Math.random()*2*Math.PI;
-        this.p = Math.random();
+        this.p = Math.random()*0.9;
         let distFromCenter = this.p*glassWidth(game.timer);
         this.py = Math.sin(this.angleToCenter)*distFromCenter;
         this.px = Math.cos(this.angleToCenter)*distFromCenter;
@@ -36,13 +36,13 @@ export class Cactus extends Phaser.Sprite {
 
         if (game.timer > this.d) {
             if (this.standing) {
-                this.loadTexture('cactus_fall');
+                this.loadTexture('cactus-fall');
                 this.animations.play('fall');
                 this.standing = false;
             }
         } else {
             if (!this.standing) {
-                this.loadTexture('cactus_dance');
+                this.loadTexture('cactus-dance');
                 this.animations.play('dance');
                 this.standing = true;
             }
